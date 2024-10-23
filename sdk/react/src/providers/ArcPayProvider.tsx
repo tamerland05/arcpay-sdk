9ad18f6451a10db352438b54b344d593b3b826f5
Bell <ds@inbox.ru>
2024-10-23 09:57:10 +0300

    Add callbacks

--- sdk/react/src/providers/ArcPayProvider.tsx
+++ sdk/react/src/providers/ArcPayProvider.tsx
@@ -1,10 +1,14 @@
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import React, { createContext, ReactNode, useState } from 'react';
+import { OrderOut } from '../types/order';
+
+type OrderChangeCallback = (order: OrderOut) => void;
 
 // Define the context value type
 interface ArcPayContextType {
   processPayment: (amount: number) => Promise<void>;
   apiBaseUrl: string;
+  onOrderChange: (callback: OrderChangeCallback) => void;
 }
 
 // Create the context
@@ -22,6 +26,14 @@ export const ArcPayProvider = ({
   children,
   apiBaseUrl,
 }: ArcPayProviderProps) => {
+  const [orderCallbacks, setOrderCallbacks] = useState<OrderChangeCallback[]>(
+    []
+  );
+
+  const onOrderChange = (callback: OrderChangeCallback) => {
+    setOrderCallbacks((prevCallbacks) => [...prevCallbacks, callback]);
+  };
+
   const [isProcessing, setIsProcessing] = useState(false);
   const [baseUrl, setBaseUrl] = useState(
     apiBaseUrl || 'https://arcpay.online/api/v1/arcpay'
@@ -39,7 +51,11 @@ export const ArcPayProvider = ({
   return (
     <TonConnectUIProvider manifestUrl="https://arcpay.online/tonconnect-manifest.json">
       <ArcPayContext.Provider
-        value={{ processPayment, apiBaseUrl: apiBaseUrl || '' }}>
+        value={{
+          processPayment,
+          apiBaseUrl: apiBaseUrl || '',
+          onOrderChange: onOrderChange,
+        }}>
         {children}
       </ArcPayContext.Provider>
     </TonConnectUIProvider>
